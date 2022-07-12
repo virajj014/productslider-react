0c50985ac682120e59d38e722d8f10abf7981645
Harshal Jain <virajj014@gmail.com>
2022-07-12 14:55:15 +0530

    Subscribe to Coders Hub Youtube

---
+++ src/Components/Imagecarousel.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import Mycard from './Mycard.js'
+import './Imagecarousel.css'
+const Imagecarousel = () => {
+    let box = document.querySelector('.product-container');
+
+    const btnpressprev = () => {
+        let width = box.clientWidth;
+        box.scrollLeft = box.scrollLeft - width;
+        console.log(width)
+    }
+
+    const btnpressnext = () => {
+        let width = box.clientWidth;
+        box.scrollLeft = box.scrollLeft + width;
+        console.log(width)
+    }
+    return (
+        <div className="product-carousel">
+            <button className="pre-btn" onClick={btnpressprev}><p>&lt;</p></button>
+            <button className="next-btn" onClick={btnpressnext}><p>&gt;</p></button>
+
+
+            <div className="product-container">
+                <Mycard cardno='1' />
+                <Mycard cardno='2' />
+                <Mycard cardno='3' />
+                <Mycard cardno='4' />
+                <Mycard cardno='5' />
+                <Mycard cardno='6' />
+                <Mycard cardno='7' />
+                <Mycard cardno='8' />
+                <Mycard cardno='9' />
+                <Mycard cardno='10' />
+                <Mycard cardno='11' />
+                <Mycard cardno='12' />
+                <Mycard cardno='13' />
+            </div>
+        </div>
+    )
+}
+
+export default Imagecarousel
---
+++ src/Components/Mycard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Mycard.css'
+const Mycard = (props) => {
+    return (
+        <div className='mycard'>Mycard No. {props.cardno}</div>
+    )
+}
+
+export default Mycard
